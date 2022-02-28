19e15ca0feeda02d4ed30b44292ece657a025d1f
Rifdah <rifdah@Rifdahs-MacBook-Air.local>
2022-02-28 22:59:42 +0700

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,15 +13,15 @@
     <section class="about">
         <article>
             <h1 class="h3-abt">About Us</h1>
-            <p class="p-abt">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lacinia consequat dolor
-                at consequat. Mauris ac porta massa, a semper orci. Morbi condimentum varius purus id rhoncus.
-                Ut porta fermentum dui ut mattis. Proin sed ullamcorper risus, id volutpat metus. In tempor
-                dui arcu, vitae commodo diam posuere nec. Nunc a sodales lorem.</p>
-                <br>
-            <p class="p-abt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lacinia consequat dolor
-                at consequat. Mauris ac porta massa, a semper orci. Morbi condimentum varius purus id rhoncus.
-                Ut porta fermentum dui ut mattis. Proin sed ullamcorper risus, id volutpat metus. In tempor
-                dui arcu, vitae commodo diam posuere nec. Nunc a sodales lorem.</p>
+            <p class="p-abt">
+                Pada bagian Home, menampilkan bagian awal dari Online Course. Pada bagian Login untuk user masuk, yang sudah melakukan registrasi.
+                Pada bagian sign-up untuk user membuat akun.
+                Pada bagian course untuk menampilkan enroll course.
+            </p>
+
+            <p class="p-abt-2">
+
+            </p>
 
         </article>
     </section>
